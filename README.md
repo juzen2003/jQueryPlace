e0cfd24d3dcfa27f7fdb0613f01064c3294391d1
Yu-Jen Chang <juzen2003@gmail.com>
2018-11-03 23:16:15 -0700

    Finish animations

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
 * hide() would set display to none
 * show() would remove display: none
 * toggle() would show if it's hide, and hide if it's show
+* slideUp() would slide up and set display to none
+* slideDown() would slide down, show the content, and set display back to original one
+* slideToggle() would show if it's hide, and hide if it's show
+* animate(), we can create customized animation to move element, make sure the passed in object has string in key value pairs
+* delay(), animations can be chained and added with delay
+* callback function can be used to time the animations, it would be executed when animation is done
--- base/js/script.js
+++ base/js/script.js
@@ -14,7 +14,50 @@ $(function() {
 
   // $(".blue-box").hide(1000);
   // $(".blue-box").show(1000);
-  $(".blue-box").toggle(2000);
+  // $(".blue-box").toggle(2000);
 
+  // $(".blue-box").slideUp(2000);
+  // $(".blue-box").slideDown(2000);
+  // $("p").hide();
+  // $("p").slideDown(2000);
+  // $(".blue-box").slideToggle(2000);
 
+  // $(".blue-box").animate({
+  //   "margin-left": "+=200px"
+  // }, 1000, "linear");
+  // $(".blue-box").animate({
+  //   "margin-left": "-=200px"
+  // }, 1000, "linear");
+
+  // $(".blue-box").animate({
+  //   "margin-left": "+=200px",
+  //   "opacity": "0",
+  //   "width": "50px",
+  //   "height": "50px",
+  //   "margin-top": "25px"
+  // }, 1000);
+  // $("p").animate({
+  //   "font-size": "20px"
+  // }, 1000);
+
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").delay(1000).fadeTo(1000, 0.5);
+  // $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut(1000).delay(1000).fadeIn(1000);
+
+  // $(".blue-box").animate({
+  //   "margin-left": "+=200px",
+  //   "opacity": "0"
+  // }, 1000, function() {
+  //   $(".green-box").animate({
+  //     "margin-left": "+=200px",
+  //     "opacity": "0"
+  //   }, 1000, function() {
+  //     $(".red-box").animate({
+  //       "margin-left": "+=200px",
+  //       "opacity": "0"
+  //     }, 1000);
+  //   });
+  // });
+
+  $(".lightbox").fadeIn(1000);
 });
