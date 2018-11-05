f0e74c209f8dee61c786508ff3ea994037da49c7
Yu-Jen Chang <juzen2003@gmail.com>
2018-11-05 12:31:53 -0800

    Finish CSS selector

--- README.md
+++ README.md
@@ -17,3 +17,29 @@
 * animate(), we can create customized animation to move element, make sure the passed in object has string in key value pairs
 * delay(), animations can be chained and added with delay
 * callback function can be used to time the animations, it would be executed when animation is done
+
+### CSS selector
+* css() would change the css properties of selected tag
+## traverse elements
+* find() would select all descendants
+* children() would select only the direct descendants (1 level only)
+* parents() would select all parents
+* parent() would select the only direct parent (1 level up only)
+* siblings() would select elements at the same level excluding the itself
+* next() would select the next siblings
+* prev() would select the previous siblings
+## filter the jQuery matched object
+* filter() could take either a jQuery selector or function that takes in index as parameter, and would filter out whoever return false and keep index that returns true
+* not() is the reverse of filter(), would filter out whoever return true and keep whoever return false
+* first() & last() would select first & last of selected object
+* eq() would take in index as parameter and select the element at input index
+
+### DOM manipulations, insert, replace and remove elemetns
+* source.append(target), append (add to the last children) target to the selected source object
+* target.appendTo(source), same as source.append(target)
+* source.prepend(target), prepend (add to the first children) target to the selected source object
+* target.prependTo(source), same as source.prepend(target)
+* source.after(target), add target as the next sibling of source
+* target.insertAfter(source), same as source.after(target)
+* source.before(target), add target as the previous sibling of source
+* target.insertBefore(source), same as source.before(target)
--- base/js/script.js
+++ base/js/script.js
@@ -59,5 +59,75 @@ $(function() {
   //   });
   // });
 
-  $(".lightbox").fadeIn(1000);
+  // $(".lightbox").fadeIn(1000);
+
+  // $("p").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $(".red-box").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("input[type='text']").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("h2, p, input").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li:first").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li:odd").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("input:text").css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $("#list").find("li").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").children("li").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").parents("body").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").parent().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").siblings().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").next().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").prev().css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $(":header").next().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("form").children(":password").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $(":password").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("input[type='password']").css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $("#list").children("li").filter(":even").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").filter(function(index){
+  //   return index % 3 === 2;
+  // }).css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $("li").first().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").last().css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $("li").eq(3).css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").eq(-2).css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $("li").not(":first").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").not(function(index){
+  //   return index % 3 === 0;
+  // }).css("background-color", "rgb(180, 180, 30, 0.8)");
+
+  // $(".red-box").after("<div class='red-box'>Red 2</div>");
+  // $(".green-box").after("<div class='green-box'>Another Green</div>");
+  // $(".blue-box").after("<div class='blue-box'>Blue Friend</div>");
+  // let colorBoxes = [{"class": "red-box", "color": "Red 2"}, {"class": "green-box", "color": "Green 2"}, {"class": "blue-box", "color": "Blue 2"}];
+  // for(let i = 0; i < colorBoxes.length; i++) {
+  //   $("." + colorBoxes[i].class).after(`<div class='${colorBoxes[i].class}'>${colorBoxes[i].color}</div>`);
+  // }
+
+  // $("ul ul:first").append("<li>I'm gonna be the last sub itme</li>");
+  // $("<li>I'm gonna be the last sub itme</li>").appendTo("ul ul:first");
+
+  // $("ul ul:first").prepend("<li>I'm gonna be the first sub itme</li>");
+  // $("<li>I'm gonna be the first sub itme</li>").prependTo("ul ul:first");
+  // $("ul ul:first").prepend(function() {
+  //   let target = "";
+  //   for(let i = 0; i < 3; i++) {
+  //     target += `<li>I'm gonna be added sub item: ${i}</li>`;
+  //   }
+  //
+  //   return target;
+  // });
+
+  // $(".red-box").after("<div class='red-box'>Red 2</div>");
+  // $(".green-box").before("<div class='green-box'>Another Green</div>");
+  // $(".blue-box").before(function(){
+  //   return "<div class='blue-box'>Blue Friend</div>";
+  // });
+  // $(".blue-box").before($(".red-box"));
+
+
+
 });
